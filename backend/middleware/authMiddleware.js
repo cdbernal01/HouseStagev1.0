96300aff233c3fa5ec73586586c1885ab8a65dc8
Cristian Bernal <cdbernal4@soy.sena.edu.co>
2024-02-03 16:46:01 -0500

    Actualizacion 2024

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,7 +3,7 @@ import asyncHandler from "./asyncHandler.js";
 import User from "../models/userModel.js";
 
 // User must be authenticated
-const protect = asyncHandler(async (req, res, next) => {
+ const protect = asyncHandler(async (req, res, next) => {
   let token;
 
   // Read JWT from the 'jwt' cookie
@@ -12,9 +12,7 @@ const protect = asyncHandler(async (req, res, next) => {
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
       req.user = await User.findById(decoded.userId).select("-password");
-
       next();
     } catch (error) {
       console.error(error);
@@ -33,7 +31,7 @@ const admin = (req, res, next) => {
     next();
   } else {
     res.status(401);
-    throw new Error("Not authorized as an admin");
+    throw new Error("Not authorized as admin");
   }
 };
 
