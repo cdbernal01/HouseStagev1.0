f6c298eb8121bb38248cb34dc13d7ec21af71ded
Cristian Bernal <cdbernal4@soy.sena.edu.co>
2024-02-27 16:12:00 -0500

    UPDATE 27/02/24

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import cookieParser from 'cookie-parser';
+dotenv.config();
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
@@ -11,7 +12,9 @@ import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 
-dotenv.config();
+const port = process.env.PORT || 5000;
+
+
 connectDB();
 
 const app = express();
@@ -42,47 +45,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('Hello World! API corriendo correctamente!');
-});
 
-/**
- * @swagger
- * /api/users:
- *   get:
- *     summary: Mapa de Sitio HOUSESTAGE
- *     description: Retorna todas las rutas disponibles de la aplicacion
- *     responses:
- *       200:
- *         description: Éxito, devuelve rutas del sitio
- */
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
 app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
-  // Implementación para obtener y devolver mapas de sitio
+
 );
 
-if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  app.use('/uploads', express.static('/var/data/uploads'));
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+const __dirname = path.resolve();
+app.use('/uploads', express.static('/var/data/uploads'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );
 } else {
-  const __dirname = path.resolve();
-  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
   app.get('/', (req, res) => {
-    res.send('API is running....');
+    res.send('Hello World! API corriendo correctamente!');
   });
 }
 
 app.use(notFound);
 app.use(errorHandler);
 
-const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Servidor en ejecución en el puerto ${port}`));
